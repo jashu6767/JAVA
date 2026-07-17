dd0bb5f7b44f0463d8bece8264cf18c08639a06a
jashu6767 <jashwanthkatthula1122@gmail.com>
2026-07-17 09:00:00 +0530

    Add practical2.java with various operations

---
+++ practical2.java
@@ -0,0 +1,74 @@
+public class practical2 { 
+    public static void main(String[] args) { 
+        int a = 10; 
+        int b = 5; 
+ // Arithmetic Operations 
+      System.out.println("Arithmetic Operations"); 
+      System.out.println("Addition = " + (a + b)); 
+      System.out.println("Subtraction = " + (a - b)); 
+      System.out.println("Multiplication = " + (a * b)); 
+      System.out.println("Division = " + (a / b)); 
+      System.out.println("Modulus = " + (a % b)); 
+      System.out.println(); 
+
+
+// Relational Operators 
+
+     System.out.println("Relational Operators"); 
+     System.out.println("a==b: " + (a == b)); 
+     System.out.println("a!=b: " + (a != b)); 
+     System.out.println("a>b: " + (a > b)); 
+     System.out.println("a<b: " + (a < b)); 
+     System.out.println("a>=b: " + (a >= b)); 
+     System.out.println("a<=b: " + (a <= b)); 
+     System.out.println(); 
+
+
+// Logical Operations
+
+        boolean x = true; 
+        boolean y = false; 
+        System.out.println("Logical Operations"); 
+        System.out.println("x&&y = " + (x && y)); 
+        System.out.println("x||y = " + (x || y)); 
+        System.out.println("!x = " + (!x)); 
+        System.out.println(); 
+
+
+ // Unary Operations 
+
+   int c = 10; 
+  System.out.println("Unary Operations"); 
+  System.out.println("Original Value = " + c); 
+  System.out.println("Unary Plus = " + (+c)); 
+  System.out.println("Unary Minus = " + (-c)); 
+  System.out.println("Pre Increment = " + (++c)); 
+  System.out.println("Post Increment = " + (c++)); 
+  System.out.println("After Post Increment = " + c); 
+  System.out.println("Pre Decrement = " + (--c)); 
+  System.out.println("Post Decrement = " + (c--)); 
+  System.out.println("After Post Decrement = " + c); 
+  System.out.println(); 
+
+
+  // Bitwise Operators
+
+
+     int p = 5; 
+      int q = 3; 
+      System.out.println("Bitwise Operators"); 
+      System.out.println("p&q = " + (p & q)); 
+      System.out.println("p|q = " + (p | q)); 
+      System.out.println("p^q = " + (p ^ q)); 
+      System.out.println("~p = " + (~p)); 
+      System.out.println("p<<1 = " + (p << 1)); 
+      System.out.println("p>>1 = " + (p >> 1)); 
+
+
+      
+    } 
+
+
+
+
+}
